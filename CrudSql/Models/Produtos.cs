352aadd72d7f3a608f23d32784885fab9b247da4
David - Valselu <David - Valselu@DAXZBR>
2025-03-16 21:31:23 -0300

    first commiit

--- CrudSql/Models/Produtos.cs
+++ CrudSql/Models/Produtos.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CrudSql.Models
+﻿namespace CrudSql.Models
 {
     class Produtos
     {
+        public int Id { get; set; }
+        public string? Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
     }
 }
